d57667dd1859bd12699ba99156838fee42395fde
amelia-loeffler <ajl28@ad.unc.edu>
2022-10-25 10:22:32 -0400

    webmap

---
+++ webmap/qgis2web_2022_10_25-10_19_36_897154/data/Little_Libraries_2246_5.js
@@ -0,0 +1,1 @@
+var json_Little_Libraries_2246_5 = {"type":"FeatureCollection","name":"Little_Libraries_2246_5","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
---
+++ webmap/qgis2web_2022_10_25-10_22_14_568377/data/Urban_Service_Area_2246_1.js
@@ -0,0 +1,1 @@
+var json_Urban_Service_Area_2246_1 = {"type":"FeatureCollection","name":"Urban_Service_Area_2246_1","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
