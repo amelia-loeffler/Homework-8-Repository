88748a28d462e4d0ed5ae957505ff51a0487e30e
amelia-loeffler <ajl28@ad.unc.edu>
2022-11-26 21:50:15 -0600

    market

---
+++ FarmersMkt/qgis2web_2022_11_26-21_49_58_671478/data/FarmersMarketCoordinates_1.js
@@ -0,0 +1,1 @@
+var json_FarmersMarketCoordinates_1 = {"type":"FeatureCollection","name":"FarmersMarketCoordinates_1","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
